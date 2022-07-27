0788bd53b6ac423c27165b7c4ab2f79a4c7cb2c3
Antonio Juričić <76628050+antoniojuricic@users.noreply.github.com>
2022-07-27 20:23:25 +0200

    Refactor data passing

--- src/components/PopularHomes/PopularHomes.tsx
+++ src/components/PopularHomes/PopularHomes.tsx
@@ -1,29 +1,17 @@
 import FeaturedSection from "../FeaturedSection/FeaturedSection";
 import AccommodationCard from "../AccommodationCard/AccommodationCard";
 import Grid from "@mui/material/Grid";
-import image from "../../assets/split.png";
 
-const AccommodationCardData = [
-  {
-    title: "Sugar & Spice Apartments",
-    location: "Split",
-    price: 75,
-    categorization: 3,
-    image: image,
-  },
-];
-
-const PopularHomes = () => {
+const PopularHomes = ({ cards }: { cards: AccommodationCard[] }) => {
   return (
     <FeaturedSection title="Homes guests love" callToAction="View all homes">
-        
-      {AccommodationCardData.map((place) => (
+      {cards.map((place) => (
         <Grid item xs={3}>
           <AccommodationCard
             title={place.title}
             location={place.location}
             image={place.image}
-            rating={place.categorization}
+            rating={place.rating}
             price={place.price}
           />
         </Grid>
--- src/components/PopularLocations/PopularLocations.tsx
+++ src/components/PopularLocations/PopularLocations.tsx
@@ -1,27 +1,19 @@
 import FeaturedSection from "../FeaturedSection/FeaturedSection";
 import CityCard from "../CityCard/CityCard";
 import Grid from "@mui/material/Grid";
-import image from "../../assets/london.png";
 
-const cityCardData = [
-  {
-    name: "London",
-    count: 5102,
-    image: image,
-  },
-];
-
-const PopularLocations = () => {
+const PopularLocations = ({ cards }: { cards: CityCard[] }) => {
   return (
-    <FeaturedSection title="Popular locations" callToAction="View all locations">
-        
-        {cityCardData.map((city) => (
-            <Grid item xs={4}>
-              <CityCard name={city.name} count={city.count} image={city.image} />
-            </Grid>
-          ))}
+    <FeaturedSection
+      title="Popular locations"
+      callToAction="View all locations"
+    >
+      {cards.map((city) => (
+        <Grid item xs={4}>
+          <CityCard name={city.name} count={city.count} image={city.image} />
+        </Grid>
+      ))}
     </FeaturedSection>
-
   );
 };
 
