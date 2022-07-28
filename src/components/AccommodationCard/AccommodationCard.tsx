1eb5c24c921cb653932b0de2125253c0e3110178
Antonio Juričić <76628050+antoniojuricic@users.noreply.github.com>
2022-07-28 10:44:48 +0200

    Add Categorization component

--- src/components/AccommodationCard/AccommodationCard.tsx
+++ src/components/AccommodationCard/AccommodationCard.tsx
@@ -1,5 +1,5 @@
 import styles from "./AccommodationCard.module.css";
-import starIcon from "../../assets/star.svg";
+import Categorization from "../Categorization";
 
 type AccommodationCard = {
   rating: number;
@@ -10,17 +10,13 @@ type AccommodationCard = {
 };
 
 const AccommodationCard = (props: AccommodationCard) => {
-  const rating = [];
-  for (var i = 0; i < props.rating; i++) {
-    rating.push(<img src={starIcon} />);
-  }
   return (
     <div className={styles.card}>
       <img className={styles.image} src={props.image} />
       <div className={styles.title}>{props.title}</div>
       <div className={styles.location}>{props.location}</div>
       <div className={styles.price}>EUR {props.price}</div>
-      <div className={styles.rating}>{rating}</div>
+      <Categorization rating={props.rating} />
     </div>
   );
 };
